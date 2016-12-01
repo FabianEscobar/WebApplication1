01e48e20f483b4adbb68b4a92553c87476d9f232
FabianEscobar <fabian.escobar@usach.cl>
2016-11-30 22:56:06 -0300

    First commit.

---
+++ src/java/facade/ProductoLocal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import model.Productos;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author zlightrider
+ */
+public interface ProductoLocal {
+
+    void addProducto(Productos producto);
+
+    void editProducto(Productos producto);
+
+    void deleteProducto(int productoId);
+
+    Productos getProducto(int productoId);
+
+    List<Productos> getAllProductos();
+    
+}
